84d7996c544f55e8c91e776e2b0c0728d9ec18d2
viniciusdds <viniciusdamasceno180@hotmail.com>
2021-09-05 21:33:28 -0300

    aula 349

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -3,9 +3,23 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:great_places/models/place.dart';
+import 'package:great_places/utils/db_util.dart';
 
 class GreatPlaces with ChangeNotifier{
    List<Place> _items = [];
+
+   Future<void> loadPlaces() async {
+     final dataList = await DBUtil.getData('places');
+     _items = dataList.map(
+        (item) => Place(
+            id: item['id'],
+            title: item['title'],
+            image: File(item['image']),
+            location: null
+        )
+     ).toList();
+     notifyListeners();
+   }
 
    List<Place> get items {
      return [..._items];
@@ -28,6 +42,11 @@ class GreatPlaces with ChangeNotifier{
      );
 
      _items.add(newPlace);
+     DBUtil.insert('places', {
+       'id': newPlace.id,
+       'title': newPlace.title,
+       'image': newPlace.image.path,
+     });
      notifyListeners();
    }
 }
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
   @override
@@ -16,9 +18,34 @@ class PlacesListScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-        child: CircularProgressIndicator(),
-      ),
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false).loadPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState == ConnectionState.waiting ?
+          Center(
+            child: CircularProgressIndicator(),
+          )
+        : Consumer<GreatPlaces>(
+          child: Center(
+            child: Text('Nenhum local cadastrado!'),
+          ),
+          builder: (ctx, greatPlaces, ch) => greatPlaces.itemsCount == 0 ?
+              ch :
+              ListView.builder(
+                itemCount: greatPlaces.itemsCount,
+                itemBuilder: (ctx, i) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: FileImage(
+                      greatPlaces.itemByIndex(i).image
+                    ),
+                  ),
+                  title: Text(greatPlaces.itemByIndex(i).title),
+                  onTap: (){
+
+                  },
+                ),
+              ),
+        ),
+      )
     );
   }
 }
